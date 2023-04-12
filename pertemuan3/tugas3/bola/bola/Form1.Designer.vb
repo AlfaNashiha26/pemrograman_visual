3f95e8f796526416994daae8d6dcf53a30013912
AlfaNashiha26 <103365359+AlfaNashiha26@users.noreply.github.com>
2023-04-12 18:29:49 +0700

    Add files via upload

---
+++ pertemuan3/tugas3/bola/bola/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        txtJari = New TextBox()
+        txtVol = New TextBox()
+        txtLuasp = New TextBox()
+        Label3 = New Label()
+        btnHitung = New Button()
+        Label4 = New Label()
+        Label5 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(87, 102)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(58, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Jari-jari"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(87, 224)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(59, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Volume"
+        ' 
+        ' txtJari
+        ' 
+        txtJari.Location = New Point(246, 102)
+        txtJari.Margin = New Padding(3, 4, 3, 4)
+        txtJari.Name = "txtJari"
+        txtJari.Size = New Size(100, 27)
+        txtJari.TabIndex = 2
+        ' 
+        ' txtVol
+        ' 
+        txtVol.Location = New Point(246, 224)
+        txtVol.Margin = New Padding(3, 4, 3, 4)
+        txtVol.Name = "txtVol"
+        txtVol.Size = New Size(100, 27)
+        txtVol.TabIndex = 3
+        ' 
+        ' txtLuasp
+        ' 
+        txtLuasp.Location = New Point(246, 274)
+        txtLuasp.Margin = New Padding(3, 4, 3, 4)
+        txtLuasp.Name = "txtLuasp"
+        txtLuasp.Size = New Size(100, 27)
+        txtLuasp.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(87, 274)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(114, 20)
+        Label3.TabIndex = 4
+        Label3.Text = "Luas Permukaan"
+        ' 
+        ' btnHitung
+        ' 
+        btnHitung.Location = New Point(256, 162)
+        btnHitung.Margin = New Padding(3, 4, 3, 4)
+        btnHitung.Name = "btnHitung"
+        btnHitung.Size = New Size(77, 36)
+        btnHitung.TabIndex = 6
+        btnHitung.Text = "Hitung"
+        btnHitung.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(105, 48)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(241, 20)
+        Label4.TabIndex = 7
+        Label4.Text = "VOLUME DAN LUAS BOLA"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(75, 325)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(315, 20)
+        Label5.TabIndex = 8
+        Label5.Text = "Create By. Alfa Nashiha(200511007)"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDark
+        ClientSize = New Size(488, 400)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(btnHitung)
+        Controls.Add(txtLuasp)
+        Controls.Add(Label3)
+        Controls.Add(txtVol)
+        Controls.Add(txtJari)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "Form1"
+        Text = "Bola - Alfa Nashiha"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtJari As TextBox
+    Friend WithEvents txtVol As TextBox
+    Friend WithEvents txtLuasp As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
