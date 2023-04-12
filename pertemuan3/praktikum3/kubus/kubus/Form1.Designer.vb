b66dbdd3a8bd598720dc42542563287c40d7459b
AlfaNashiha26 <103365359+AlfaNashiha26@users.noreply.github.com>
2023-04-12 21:42:01 +0700

    Add files via upload

---
+++ pertemuan3/praktikum3/kubus/kubus/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label10 = New Label()
+        Label9 = New Label()
+        btnHitung = New Button()
+        txtLuasP = New TextBox()
+        Label8 = New Label()
+        txtVolum = New TextBox()
+        Label7 = New Label()
+        txtSisi = New TextBox()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label10.Location = New Point(65, 358)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(315, 20)
+        Label10.TabIndex = 17
+        Label10.Text = "Create By. Alfa Nashiha(200511007)"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label9.Location = New Point(51, 71)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(374, 20)
+        Label9.TabIndex = 16
+        Label9.Text = "VOLUME DAN LUAS PERMUKAAN KUBUS"
+        ' 
+        ' btnHitung
+        ' 
+        btnHitung.Location = New Point(248, 198)
+        btnHitung.Margin = New Padding(3, 4, 3, 4)
+        btnHitung.Name = "btnHitung"
+        btnHitung.Size = New Size(91, 36)
+        btnHitung.TabIndex = 15
+        btnHitung.Text = "Hitung"
+        btnHitung.UseVisualStyleBackColor = True
+        ' 
+        ' txtLuasP
+        ' 
+        txtLuasP.Location = New Point(248, 304)
+        txtLuasP.Margin = New Padding(3, 4, 3, 4)
+        txtLuasP.Name = "txtLuasP"
+        txtLuasP.Size = New Size(100, 27)
+        txtLuasP.TabIndex = 14
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(92, 304)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(114, 20)
+        Label8.TabIndex = 13
+        Label8.Text = "Luas Permukaan"
+        ' 
+        ' txtVolum
+        ' 
+        txtVolum.Location = New Point(248, 258)
+        txtVolum.Margin = New Padding(3, 4, 3, 4)
+        txtVolum.Name = "txtVolum"
+        txtVolum.Size = New Size(100, 27)
+        txtVolum.TabIndex = 12
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(92, 258)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(59, 20)
+        Label7.TabIndex = 11
+        Label7.Text = "Volume"
+        ' 
+        ' txtSisi
+        ' 
+        txtSisi.Location = New Point(248, 130)
+        txtSisi.Margin = New Padding(3, 4, 3, 4)
+        txtSisi.Name = "txtSisi"
+        txtSisi.Size = New Size(100, 27)
+        txtSisi.TabIndex = 10
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(92, 130)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(31, 20)
+        Label6.TabIndex = 9
+        Label6.Text = "Sisi"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDark
+        ClientSize = New Size(476, 452)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(btnHitung)
+        Controls.Add(txtLuasP)
+        Controls.Add(Label8)
+        Controls.Add(txtVolum)
+        Controls.Add(Label7)
+        Controls.Add(txtSisi)
+        Controls.Add(Label6)
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "Form1"
+        Text = "Kubus - Alfa Nashiha"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtLuasP As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtVolum As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtSisi As TextBox
+    Friend WithEvents Label6 As Label
+End Class
---
+++ pertemuan3/praktikum3/layang_layang/layang_layang/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label10 = New Label()
+        txtKeliling = New TextBox()
+        Label9 = New Label()
+        txtLuas = New TextBox()
+        Label8 = New Label()
+        btnHitung = New Button()
+        txtSisiD = New TextBox()
+        Label7 = New Label()
+        txtSIsiC = New TextBox()
+        Label6 = New Label()
+        txtSisiB = New TextBox()
+        Label5 = New Label()
+        txtSisiA = New TextBox()
+        Label4 = New Label()
+        txtD2 = New TextBox()
+        Label3 = New Label()
+        txtD1 = New TextBox()
+        Label2 = New Label()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Microsoft Sans Serif", 12.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label10.Location = New Point(117, 388)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(367, 25)
+        Label10.TabIndex = 39
+        Label10.Text = "Create By. Alfa Nashiha(200511007)"
+        ' 
+        ' txtKeliling
+        ' 
+        txtKeliling.Location = New Point(408, 328)
+        txtKeliling.Margin = New Padding(3, 4, 3, 4)
+        txtKeliling.Name = "txtKeliling"
+        txtKeliling.Size = New Size(100, 27)
+        txtKeliling.TabIndex = 38
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(324, 334)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(59, 20)
+        Label9.TabIndex = 37
+        Label9.Text = "Keliling"
+        ' 
+        ' txtLuas
+        ' 
+        txtLuas.Location = New Point(181, 328)
+        txtLuas.Margin = New Padding(3, 4, 3, 4)
+        txtLuas.Name = "txtLuas"
+        txtLuas.Size = New Size(100, 27)
+        txtLuas.TabIndex = 36
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(91, 331)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(38, 20)
+        Label8.TabIndex = 35
+        Label8.Text = "Luas"
+        ' 
+        ' btnHitung
+        ' 
+        btnHitung.Location = New Point(242, 272)
+        btnHitung.Margin = New Padding(3, 4, 3, 4)
+        btnHitung.Name = "btnHitung"
+        btnHitung.Size = New Size(126, 35)
+        btnHitung.TabIndex = 34
+        btnHitung.Text = "Hitung"
+        btnHitung.UseVisualStyleBackColor = True
+        ' 
+        ' txtSisiD
+        ' 
+        txtSisiD.Location = New Point(408, 219)
+        txtSisiD.Margin = New Padding(3, 4, 3, 4)
+        txtSisiD.Name = "txtSisiD"
+        txtSisiD.Size = New Size(100, 27)
+        txtSisiD.TabIndex = 33
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(324, 226)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(46, 20)
+        Label7.TabIndex = 32
+        Label7.Text = "Sisi D"
+        ' 
+        ' txtSIsiC
+        ' 
+        txtSIsiC.Location = New Point(408, 159)
+        txtSIsiC.Margin = New Padding(3, 4, 3, 4)
+        txtSIsiC.Name = "txtSIsiC"
+        txtSIsiC.Size = New Size(100, 27)
+        txtSIsiC.TabIndex = 31
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(324, 166)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(44, 20)
+        Label6.TabIndex = 30
+        Label6.Text = "Sisi C"
+        ' 
+        ' txtSisiB
+        ' 
+        txtSisiB.Location = New Point(408, 104)
+        txtSisiB.Margin = New Padding(3, 4, 3, 4)
+        txtSisiB.Name = "txtSisiB"
+        txtSisiB.Size = New Size(100, 27)
+        txtSisiB.TabIndex = 29
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(324, 111)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(44, 20)
+        Label5.TabIndex = 28
+        Label5.Text = "Sisi B"
+        ' 
+        ' txtSisiA
+        ' 
+        txtSisiA.Location = New Point(181, 218)
+        txtSisiA.Margin = New Padding(3, 4, 3, 4)
+        txtSisiA.Name = "txtSisiA"
+        txtSisiA.Size = New Size(100, 27)
+        txtSisiA.TabIndex = 27
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(91, 224)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(45, 20)
+        Label4.TabIndex = 26
+        Label4.Text = "Sisi A"
+        ' 
+        ' txtD2
+        ' 
+        txtD2.Location = New Point(181, 159)
+        txtD2.Margin = New Padding(3, 4, 3, 4)
+        txtD2.Name = "txtD2"
+        txtD2.Size = New Size(100, 27)
+        txtD2.TabIndex = 25
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(91, 166)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(82, 20)
+        Label3.TabIndex = 24
+        Label3.Text = "Dianogal 2"
+        ' 
+        ' txtD1
+        ' 
+        txtD1.Location = New Point(181, 105)
+        txtD1.Margin = New Padding(3, 4, 3, 4)
+        txtD1.Name = "txtD1"
+        txtD1.Size = New Size(100, 27)
+        txtD1.TabIndex = 23
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(91, 111)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(82, 20)
+        Label2.TabIndex = 22
+        Label2.Text = "Diagonal 1"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Microsoft Sans Serif", 12.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(98, 50)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(410, 25)
+        Label1.TabIndex = 21
+        Label1.Text = "LUAS DAN KELILING LAYANG-LAYANG"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDark
+        ClientSize = New Size(599, 468)
+        Controls.Add(Label10)
+        Controls.Add(txtKeliling)
+        Controls.Add(Label9)
+        Controls.Add(txtLuas)
+        Controls.Add(Label8)
+        Controls.Add(btnHitung)
+        Controls.Add(txtSisiD)
+        Controls.Add(Label7)
+        Controls.Add(txtSIsiC)
+        Controls.Add(Label6)
+        Controls.Add(txtSisiB)
+        Controls.Add(Label5)
+        Controls.Add(txtSisiA)
+        Controls.Add(Label4)
+        Controls.Add(txtD2)
+        Controls.Add(Label3)
+        Controls.Add(txtD1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "Form1"
+        Text = "Layang layang - Alfa Nashiha"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtKeliling As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtLuas As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtSisiD As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtSIsiC As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtSisiB As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtSisiA As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtD2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtD1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
