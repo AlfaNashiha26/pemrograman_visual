26be1869fb316aa4cba1158db3adcd074aea3eff
AlfaNashiha26 <103365359+AlfaNashiha26@users.noreply.github.com>
2023-07-21 05:53:56 +0700

    Add files via upload

---
+++ pertemuan15/login_api_vbnet-main/client/AppMultiLogin/AppMultiLogin/Penjualan.vb
@@ -0,0 +1,6 @@
+﻿Public Class Penjualan
+    Public Property kode As String
+    Public Property nama As String
+    Public Property harga As String
+    Public Property payment As String
+End Class
