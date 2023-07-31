63edef97b8ab165255ac0c63967a7f06c7cd55bb
AlfaNashiha26 <103365359+AlfaNashiha26@users.noreply.github.com>
2023-07-31 09:03:56 +0700

    Delete Tugas_Akhir directory

--- Tugas_Akhir/client/AppMultiLogin/AppMultiLogin/Penjualan.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Penjualan
-    Public Property kode As String
-    Public Property nama As String
-    Public Property harga As String
-    Public Property payment As String
-End Class
--- Tugas_Akhir/client/AppMultiLogin/AppMultiLogin/Sparepart.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Sparepart
-    Public Property kode As String
-    Public Property nama As String
-    Public Property harga As String
-    Public Property stok As String
-
-
-End Class
